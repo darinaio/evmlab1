9998beeb4aad45698124278ca9817d057d20e371
darinaio <darinaionovaser2005@gmail.com>
2024-09-16 22:02:07 +0700

    Add files via upload

---
+++ Source.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <intrin.h>
+#define N 1690000000
+
+double exp_maclaurin(double x) {
+    double sum = 0.0;
+    double term = 1;
+    for (double n = 0; n < N; n++) {
+        sum += term;
+        term = term * x / (n + 1.0);
+    }
+    return sum;
+}
+
+int main() {
+    double x;
+    scanf("%lf", &x);
+
+    long double time = (long double)__rdtsc();
+    double result = exp_maclaurin(x);
+
+    time = (long double)__rdtsc() - time;
+    printf("%.10lf", time / 2300000000.0);
+    return 0;
+}
